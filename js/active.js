a8fa2b781dba5b3f0d023d2520997cb8d30e4326
Andrei Homentcovschi <ahomentc@gmail.com>
2020-08-09 23:24:13 -0700

    update

--- js/active.js
+++ js/active.js
@@ -40,6 +40,17 @@
 
 
 $('document').ready(function(){
+  $("#slideshow > div:gt(0)").hide();
+
+  setInterval(function() {
+    $('#slideshow > div:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#slideshow');
+  }, 6000);
+
   //======> Example Slider
   AOS.init();
   $('.example-slider').slick({
